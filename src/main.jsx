2fdfea001690a0ab5de08693fb103fe0a122ecc3
joa7b <joa7@protonmail.com>
2022-10-18 13:15:32 -0300

    delete some routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AllProducts from "./pages/getAllproducts";
-import Fruits from "./pages/fruits/index";
-import Grocery from "./pages/grocery/index";
-import Meat from "./pages/meat/index";
-import Cleaning from "./pages/cleaning/index";
 import GetByIdProduct from "./pages/getByIdProduct";
 import Cart from "./pages/cart/index";
 import Login from "./pages/login/index";
@@ -20,10 +16,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route path="/login" element={<Login />} />
       <Route path="/carrinho" element={<Cart />} />
       <Route path="/produto" element={<GetByIdProduct />} />
-      <Route path="/produtos/limpeza" element={<Cleaning />} />
-      <Route path="/produtos/carnes" element={<Meat />} />
-      <Route path="/produtos/mercearia" element={<Grocery />} />
-      <Route path="/produtos/frutas" element={<Fruits />} />
       <Route path="/" element={<AllProducts />} />
     </Routes>
     <Footer />
